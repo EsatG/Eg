4408d868f8cdfdc601f8091f90d276cc520aad55
EsatG <gorgunesad@gmail.com>
2020-04-24 13:41:41 -0400

    hello githubbb

--- src/MyStudies/study1.java
+++ src/MyStudies/study1.java
@@ -3,6 +3,6 @@ package MyStudies;
 public class study1 {
 
     public static void main(String[] args) {
-        System.out.println("hello github ");
+        System.out.println("hello github");
     }
 }
