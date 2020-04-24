1dbd45656793725f23f4049225d9d8fdf945e115
EsatG <gorgunesad@gmail.com>
2020-04-24 14:32:07 -0400

    Practice1

---
+++ src/gitPractice1/Practice1.java
@@ -0,0 +1,17 @@
+package gitPractice1;
+
+public class Practice1 {
+    /*
+    git int =>> create a repositary
+    git add. =>> add files to staging area
+    git add --all =>> add all files which is selected
+    git commit -m "message"
+    git push =>> send file to github
+    git status =>> to understand which step you are in
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Happy to see yu B18");
+
+    }
+}
