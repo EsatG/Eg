1554eacc71bbf6aefb2f4399b2f620c6f03476fd
EsatG <gorgunesad@gmail.com>
2020-04-30 11:35:10 -0400

    Day31 Class

---
+++ src/day31_BulkOperations/Bulk_ContainsAll.java
@@ -0,0 +1,54 @@
+package day31_BulkOperations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Bulk_ContainsAll {
+    /*
+    Bulk operations:
+         containsAll (CollectionType):
+
+         addAll (CollectionType):
+         removeAll (CollectionType):
+         retainAll (CollectionType):
+     */
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+                    list1.add(10);
+                    list1.add(20);
+                    list1.add(30);
+                    list1.add(40);
+        // if 10,20,60 all objects are exist in arraylist =>> true, otherwise =>> false
+           /*
+            boolean r1 = list1.contains(10);
+            boolean r2 = list1.contains(20);
+            boolean r3 = list1.contains(60);
+
+            boolean result = r1 == true && r2 == true && r3 == true;
+
+        System.out.println(result);
+
+            */
+
+        boolean r1 = list1.containsAll(Arrays.asList(10,20,60));
+        // Array.asList(object1,object2...)=>> return as an array list
+        System.out.println(r1);
+
+        Integer [] data = {10,30,40};
+       boolean r2 =  list1.containsAll(Arrays.asList(data));
+        System.out.println(r2);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
