b0cbfd6aaf29a26610bb084752ebd2f1f8746b84
EsatG <gorgunesad@gmail.com>
2020-05-05 14:00:29 -0400

    Day34 Class

--- src/day34_CustomClass/Dog.java
+++ src/day34_CustomClass/Dog.java
@@ -8,6 +8,22 @@ public class Dog {
     String color;
     String name;
 
+    public void eating(String food){
+        System.out.println(name+" is eating "+food);
+    }
+
+    public void drinking(String drink){
+        System.out.println(name+" is drinking "+drink);
+    }
+
+    public void playing(String toy){
+        System.out.println(name+" is playing "+toy);
+    }
+
+    public void studying(String subject){
+        System.out.println(name+" is studying "+subject);
+    }
+
     public void setDogInfo(String breed, String size, int age, String color,String name){
         this.breed = breed;
         this.size = size;
